2ce5db8ca4e97fc4b524621aaf409f446cf9673b
Gleb V. Zhegulin <gleboceanborn@gmail.com>
2022-05-06 19:51:41 +0300

    second commit

--- encrypt.rb
+++ encrypt.rb
@@ -1,30 +1,30 @@
-# Программа для шифрования слова с помощью механизма MD5 или SHA1
+# Программа для шифрования слова с помощью механизма MD5, SHA1 или SHA2
 
-# Подключаем встроенную в ruby библиотеку с модулем Digest
-#
 # https://ruby-doc.org/stdlib-2.4.0/libdoc/digest/rdoc/Digest.html
-require "digest"
+require 'digest'
 
 # Запрашиваем у пользователя строку и записываем её в переменную string
-puts "Введите слово или фразу для шифрования:"
-string = STDIN.gets.chomp
+puts 'Введите слово или фразу для шифрования:'
+string = $stdin.gets.chomp
 
 # Запрашиваем у пользователя метод шифрования
-puts "Каким способом зашифровать:"
-puts "1. MD5"
-puts "2. SHA1"
-encrypt_method = STDIN.gets.to_i
+puts 'Каким способом зашифровать:'
+puts '1. MD5'
+puts '2. SHA1'
+puts '3. SHA2'
+encrypt_method = $stdin.gets.to_i
 
-until encrypt_method.between?(1, 2)
-    puts "Выберите 1 или 2..."
-    encrypt_method = STDIN.gets.to_i
+until encrypt_method.between?(1, 3)
+  puts 'Выберите 1, 2 или 3:'
+  encrypt_method = $stdin.gets.to_i
 end
 
-puts "Вот что получилось:"
+puts 'Вот что получилось:'
 
 # Выводим зашифрованную строку в зависимости от выбора пользователя
 
 case encrypt_method
 when 1 then puts Digest::MD5.hexdigest(string)
 when 2 then puts Digest::SHA1.hexdigest(string)
+when 3 then puts Digest::SHA2.hexdigest(string)
 end
